c38bf0bc21fecc46b7eb8247297b99f8e78e0dc0
hoangthixuanhoa <71135030+hoangthixuanhoa@users.noreply.github.com>
2025-01-02 16:32:09 +0700

    a

--- MCP/define_reg.c
+++ MCP/define_reg.c
@@ -20,11 +20,32 @@ typedef struct
 
 } RCC_TYPE;
 
+typedef struct
+{
+    uint32_t GPIOC_CRL;
+    uint32_t GPIOC_CRH;
+    uint32_t GPIOC_IDR;
+    uint32_t GPIOC_ODR;
+    uint32_t GPIOC_BSRR;
+    uint32_t GPIOC_BRR;
+    uint32_t GPIOC_LCKR;
+} GPIOC_TYPE;
+
+
 #define RCC (RCC_TYPE *)(0x40021000U)
 
 #define RCC_APB2ENR_IOPCE_SHIFT (4u)
 #define RCC_APB2ENR_IOPCE_MASK (0x10u)
 #define RCC_APB2ENR_IOPCE(x) (((x) << RCC_APB2ENR_IOPCE_SHIFT) & RCC_APB2ENR_IOPCE_MASK)
+
+
+#define GPIOC (GPIOC_TYPE *)(0x40011000U)
+
+#define GPIOC_ODR_IOPCE_SHIFT (13u)
+#define GPIOC_ODR_IOPCE_MASK (0x1000u)
+#define GPIOC_ODR_IOPCE(x) (((x) << GPIOC_ODR_IOPCE_SHIFT) & GPIOC_ODR_IOPCE_MASK)
+
+
 
 int main()
 {
